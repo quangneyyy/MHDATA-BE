a6815c6477eb23799416c1813d8c55d6fc11a9c5
quangneyyy <quang.phanvinh@meowhouse.vn>
2022-11-12 18:33:03 +0700

    quangneyyy
    project-update

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+const host = "meowhouse.database.windows.net";
+const user = "dev";
+const database = "meowhouseweb";
+const password = "Meowhousetechnology2022123";
+const port = 3306;
+const port = 3305;
+const port = 3304;
+const port = 3303;
+const port = 3302;
+
+export { host, user, database, password, port };
